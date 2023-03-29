89ff6b939e8bb304c3dc3f428ffb8f791034452b
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-29 15:45:59 -0500

    ChatGPT?

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,43 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import createBareServer from '@tomphttp/bare-server-node'
+import express from 'express'
+import chalk from 'chalk'
+import block from './blocklist/block.json' assert { type: 'json' }
 
-import createBareServer from '@tomphttp/bare-server-node';
-import express from 'express';
-import chalk from "chalk";
-import block from "./blocklist/block.json" assert { type: "json" };
+const bareServer = createBareServer('/not-sus-server/')
 
 const setupProxy = {
   name: 'setup-proxy-plugin',
   async configureServer(server) {
-    const bareServer = createBareServer("/not-sus-server/");
-
-    server.middlewares.use((req, res, next) => {
-      if(bareServer.shouldRoute(req)) {
-        if (block.includes(req.headers["x-bare-host"])) {
+    server.httpServer.on('request', (req, res) => {
+      if (bareServer.shouldRoute(req)) {
+        if (block.includes(req.headers['x-bare-host'])) {
           return res.end(`{
-              "id": "error.Blocked",
-              "message": "Header was blocked by the owner of this site. Is it a porn site?",
-         `);
+            "id": "error.Blocked",
+            "message": "Header was blocked by the owner of this site. Is this a porn site?",
+          }`)
         }
-    bareServer.routeRequest(req, res);
-  } else {
-    server.middlewares.use("/URIconfig", (req, res) => {
-      res.end(JSON.stringify({
-        DC: process.env['INVITE_URL'] || "example.com",
-        CH: process.env['CHATBOX_URL'] || "example.com"
-      }));
-    });
+        bareServer.routeRequest(req, res)
+      } else {
+        server.middlewares(req, res, () => {})
+      }
+    })
 
-    server.middlewares.use(express.static("./static"));
-  }
+    server.middlewares.use('/URIconfig', (req, res) => {
+      res.end(
+        JSON.stringify({
+          DC: process.env['INVITE_URL'] || 'example.com',
+          CH: process.env['CHATBOX_URL'] || 'example.com',
+        })
+      )
+    })
+
+    server.middlewares.use(express.static("./static"))
+  },
 }
-)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -46,8 +51,8 @@ export default defineConfig({
         },
         titleProp: true,
         ref: true,
-      }
+      },
     }),
-    setupProxy
-  ]
+    setupProxy,
+  ],
 })

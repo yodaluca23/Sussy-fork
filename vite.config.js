031db3d78bf3878c427cd0eb8d4f80ebf7ed633a
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-30 15:49:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import createBareServer from '@tomphttp/bare-server-node'
+import express from 'express'
 import dns from 'dns'
-
-import createBareServer from '@tomphttp/bare-server-node';
-import express from 'express';
 
 const setupProxy = {
   name: 'setup-proxy-plugin',
   async configureServer(server) {
-    const bareServer = createBareServer("/not-sus-server/");
+    const bareServer = createBareServer("/not-sus-server/")
 
     server.middlewares.use((req, res, next) => {
       if(bareServer.shouldRoute(req)) bareServer.routeRequest(req, res); else next();
-    });
+    })
 
     server.middlewares.use("/URIconfig", (req, res) => {
       res.end(JSON.stringify({
         DC: process.env['INVITE_URL'] || "example.com",
         CH: process.env['CHATBOX_URL'] || "example.com"
-      }));
-    });
+      }))
+    })
 
-    server.middlewares.use(express.static("./static"));
+    server.middlewares.use(express.static("./static"))
+  },
+  async configureServer(server) {
+    const dnsServer = process.env['DNS'] || '1.1.1.1'
+
+    const customResolver = new dns.promises.Resolver()
+    customResolver.setServers([dnsServer])
+
+    server.middlewares.use(async (req, res, next) => {
+      try {
+        const domain = new URL(req.url).hostname
+        await customResolver.resolve4(domain)
+        next()
+      } catch (err) {
+        console.error(`Blocked request to ${req.url}: ${err.message}`)
+        res.status(403).send(`Access denied: ${req.url} is blocked by the website host. Is this a porn site?`)
+      }
+    })
   }
 }
-
-// Custom resolver that enforces DNS filtering
-const customResolver = {
-  name: 'custom-resolver-plugin',
-  async resolveId(id, importer) {
-    const dnsServer = process.env.DNS || '1.1.1.1';
-    const options = { family: 4, all: false, hints: dns.ADDRCONFIG | dns.V4MAPPED };
-    try {
-      await dns.promises.lookup(id, options);
-      return null; // allow resolution using default algorithm
-    } catch (err) {
-      return `Cannot resolve ${id}`; // block resolution
-    }
-  }
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -56,7 +57,6 @@ export default defineConfig({
         ref: true,
       }
     }),
-    setupProxy,
-    customResolver
+    setupProxy
   ]
-});
+})

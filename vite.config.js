4155e0b883d6e19d2c6ef1bfc9a8e59d888d40f0
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-30 13:19:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import dns from 'dns';
-import createBareServer from '@tomphttp/bare-server-node';
-import express from 'express';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import createProxyMiddleware from 'http-proxy-middleware'
+import dns from 'dns'
 
 const customResolver = (url, callback) => {
-  const dnsServer = process.env.DNS || '1.1.1.1';
+  const dnsServer = process.env.PROXY_DNS || '1.1.1.1';
   const resolver = new dns.Resolver();
   resolver.setServers([dnsServer]);
   resolver.resolve4(url.hostname, (err, addresses) => {
@@ -19,76 +17,6 @@ const customResolver = (url, callback) => {
     }
   });
 };
-
-const setupProxy = {
-  name: 'setup-proxy-plugin',
-  async configureServer(server) {
-    const bareServer = createBareServer('/not-sus-server/');
-
-    server.middlewares.use((req, res, next) => {
-      if (bareServer.shouldRoute(req)) {
-        bareServer.routeRequest(req, res);
-      } else {
-        next();
-      }
-    });
-
-    server.middlewares.use('/URIconfig', (req, res) => {
-      res.end(
-        JSON.stringify({
-          DC: process.env.INVITE_URL || 'example.com',
-          CH: process.env.CHATBOX_URL || 'example.com'
-        })
-      );
-    });
-
-    server.middlewares.use(express.static('./static'));
-
-    const proxy = createProxyMiddleware({
-      target: 'http://localhost:5000',
-      changeOrigin: true,
-      onProxyReq: logRequestHeaders,
-      selfHandleResponse: true,
-      cookieDomainRewrite: {
-        '*': ''
-      },
-      cookiePathRewrite: {
-        '*': '/'
-      },
-      resolver: customResolver // <-- Add this line to use the custom resolver
-    });
-
-    server.middlewares.use(proxy);
-  }
-};
-
-function logRequestHeaders(proxyReq) {
-  console.log(`Proxying ${proxyReq.path} to ${proxyReq.proxy.host}:${proxyReq.proxy.port}`);
-  console.log('Request Headers:');
-  console.log(proxyReq.headers);
-}
-
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        prettier: false,
-        svgo: false,
-        svgoConfig: {
-          plugins: [{ removeViewBox: false }]
-        },
-        titleProp: true,
-        ref: true
-      }
-    }),
-    setupProxy
-  ]
-});
-
-    });
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -104,7 +32,26 @@ export default defineConfig({
         titleProp: true,
         ref: true,
       }
-    }),
-    setupProxy
-  ]
+    })
+  ],
+  server: {
+    middleware: [
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        onProxyReq: logRequestHeaders,
+        selfHandleResponse: true,
+        cookieDomainRewrite: {
+          '*': ''
+        },
+        cookiePathRewrite: {
+          '*': '/'
+        },
+        resolver: customResolver
+      })
+    ],
+    fs: {
+      strict: true
+    }
+  }
 })

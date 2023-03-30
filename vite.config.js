4022852ccf932704aa4d3df222b1c22c452a4082
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-30 13:14:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import dns from 'dns';
 import createBareServer from '@tomphttp/bare-server-node';
 import express from 'express';
-import dns from 'dns';
-
-const familyProtectionDNS = process.env.DNS || '1.1.1.1';  // Sets DNS to the envioment variable, if not supplied, default to CloudFlare "1.1.1.1"
 
 const customResolver = (url, callback) => {
-  dns.resolve4(url.hostname, familyProtectionDNS, (err, addresses) => {
+  const dnsServer = process.env.DNS || '1.1.1.1';
+  const resolver = new dns.Resolver();
+  resolver.setServers([dnsServer]);
+  resolver.resolve4(url.hostname, (err, addresses) => {
     if (err) {
       callback(err);
     } else {
@@ -22,33 +23,69 @@ const customResolver = (url, callback) => {
 const setupProxy = {
   name: 'setup-proxy-plugin',
   async configureServer(server) {
-    const bareServer = createBareServer("/not-sus-server/");
+    const bareServer = createBareServer('/not-sus-server/');
 
     server.middlewares.use((req, res, next) => {
-      if(bareServer.shouldRoute(req)) bareServer.routeRequest(req, res); else next();
+      if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+      } else {
+        next();
+      }
     });
 
-    server.middlewares.use("/URIconfig", (req, res) => {
-      res.end(JSON.stringify({
-        DC: process.env['INVITE_URL'] || "example.com",
-        CH: process.env['CHATBOX_URL'] || "example.com"
-      }));
+    server.middlewares.use('/URIconfig', (req, res) => {
+      res.end(
+        JSON.stringify({
+          DC: process.env.INVITE_URL || 'example.com',
+          CH: process.env.CHATBOX_URL || 'example.com'
+        })
+      );
     });
 
-    server.middlewares.use(express.static("./static"));
+    server.middlewares.use(express.static('./static'));
 
-    server.middlewares.use((req, res, next) => {
-      const originalResolve = req._proxyRequestOptions.lookup;
-      req._proxyRequestOptions.lookup = customResolver.bind(null, req._proxyRequestOptions.url);
-      originalResolve(req._proxyRequestOptions.hostname, (err, address) => {
-        req._proxyRequestOptions.lookup = originalResolve;
-        if (err) {
-          next(err);
-        } else {
-          req._proxyRequestOptions.host = address;
-          next();
-        }
-      });
+    const proxy = createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+      onProxyReq: logRequestHeaders,
+      selfHandleResponse: true,
+      cookieDomainRewrite: {
+        '*': ''
+      },
+      cookiePathRewrite: {
+        '*': '/'
+      },
+      resolver: customResolver // <-- Add this line to use the custom resolver
+    });
+
+    server.middlewares.use(proxy);
+  }
+};
+
+function logRequestHeaders(proxyReq) {
+  console.log(`Proxying ${proxyReq.path} to ${proxyReq.proxy.host}:${proxyReq.proxy.port}`);
+  console.log('Request Headers:');
+  console.log(proxyReq.headers);
+}
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        prettier: false,
+        svgo: false,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }]
+        },
+        titleProp: true,
+        ref: true
+      }
+    }),
+    setupProxy
+  ]
+});
+
     });
   }
 }

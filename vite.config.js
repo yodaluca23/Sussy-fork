81e0d294366d681a89096eba5c5ffa8698710f4e
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-30 13:32:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import createProxyMiddleware from 'http-proxy-middleware'
+import createBareServer from '@tomphttp/bare-server-node'
+import express from 'express'
 import dns from 'dns'
 
-const customResolver = (url, callback) => {
-  const dnsServer = process.env.DNS || '1.1.1.1';
-  const resolver = new dns.Resolver();
-  resolver.setServers([dnsServer]);
-  resolver.resolve4(url.hostname, (err, addresses) => {
-    if (err) {
-      callback(err);
-    } else {
-      url.host = addresses[0];
-      callback(null, url);
-    }
-  });
-};
+const setupProxy = {
+  name: 'setup-proxy-plugin',
+  async configureServer(server) {
+    const bareServer = createBareServer("/not-sus-server/")
+
+    server.middlewares.use((req, res, next) => {
+      if(bareServer.shouldRoute(req)) bareServer.routeRequest(req, res); else next();
+    })
+
+    server.middlewares.use("/URIconfig", (req, res) => {
+      res.end(JSON.stringify({
+        DC: process.env['INVITE_URL'] || "example.com",
+        CH: process.env['CHATBOX_URL'] || "example.com"
+      }))
+    })
+
+    server.middlewares.use(express.static("./static"))
+  },
+  async configureServer(server) {
+    const dnsServer = process.env['DNS'] || '1.1.1.1'
+
+    const customResolver = new dns.promises.Resolver()
+    customResolver.setServers([dnsServer])
+
+    server.middlewares.use(async (req, res, next) => {
+      try {
+        const domain = new URL(req.url).hostname
+        await customResolver.resolve4(domain)
+        next()
+      } catch (err) {
+        console.error(`Blocked request to ${req.url}: ${err.message}`)
+        res.status(403).send(`Access denied: ${req.url} is blocked by DNS filtering`)
+      }
+    })
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,26 +56,7 @@ export default defineConfig({
         titleProp: true,
         ref: true,
       }
-    })
-  ],
-  server: {
-    middleware: [
-      createProxyMiddleware({
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        onProxyReq: logRequestHeaders,
-        selfHandleResponse: true,
-        cookieDomainRewrite: {
-          '*': ''
-        },
-        cookiePathRewrite: {
-          '*': '/'
-        },
-        resolver: customResolver
-      })
-    ],
-    fs: {
-      strict: true
-    }
-  }
+    }),
+    setupProxy
+  ]
 })

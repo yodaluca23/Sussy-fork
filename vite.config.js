6d60430310a317fe02e083aaec673bde02bbc6cd
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-30 14:57:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import createBareServer from '@tomphttp/bare-server-node'
-import express from 'express'
-import dns from 'dns'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import createBareServer from '@tomphttp/bare-server-node';
+import express from 'express';
+import dns from 'dns';
 
 const setupProxy = {
   name: 'setup-proxy-plugin',
   async configureServer(server) {
-    const bareServer = createBareServer("/not-sus-server/")
+    const bareServer = createBareServer("/not-sus-server/");
 
     server.middlewares.use((req, res, next) => {
       if(bareServer.shouldRoute(req)) bareServer.routeRequest(req, res); else next();
-    })
+    });
 
     server.middlewares.use("/URIconfig", (req, res) => {
       res.end(JSON.stringify({
         DC: process.env['INVITE_URL'] || "example.com",
         CH: process.env['CHATBOX_URL'] || "example.com"
-      }))
-    })
+      }));
+    });
 
-    server.middlewares.use(express.static("./static"))
-  },
-  async configureServer(server) {
-    const dnsServer = process.env['DNS'] || '1.1.1.1'
-
-    const customResolver = new dns.promises.Resolver()
-    customResolver.setServers([dnsServer])
-
-    server.middlewares.use(async (req, res, next) => {
-      try {
-        const domain = new URL(req.url).hostname
-        await customResolver.resolve4(domain)
-        next()
-      } catch (err) {
-        console.error(`Blocked request to ${req.url}: ${err.message}`)
-        res.status(403).send(`Access denied: ${req.url} is blocked by DNS filtering`)
-      }
-    })
+    server.middlewares.use(express.static("./static"));
   }
 }
+
+const customResolver = {
+  name: 'custom-resolver-plugin',
+  async configureServer(server) {
+    const dnsServer = process.env['DNS'] || "1.1.1.1";
+    const resolver = new dns.promises.Resolver();
+    resolver.setServers([dnsServer]);
+
+    server.middlewares.use((req, res, next) => {
+      const urlObj = new URL(req.url, `http://${req.headers.host}`);
+      const hostname = urlObj.hostname;
+
+      resolver.resolve(hostname)
+        .then(() => next())
+        .catch(() => res.sendStatus(404));
+    });
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -57,6 +58,7 @@ export default defineConfig({
         ref: true,
       }
     }),
-    setupProxy
+    setupProxy,
+    customResolver
   ]
-})
+});

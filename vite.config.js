c5b4c91c65c6b180ae0a5fb0b5c0d3384f6d0446
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2023-03-31 12:18:36 +0000

    mabye?

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import createBareServer from '@tomphttp/bare-server-node';
 import express from 'express';
-import dns from 'dns';
+import net from 'net';
 
 const setupProxy = {
   name: 'setup-proxy-plugin',
   async configureServer(server) {
-    const bareServer = createBareServer("/not-sus-server/");
+    const bareServer = createBareServer('/not-sus-server/');
 
     server.middlewares.use((req, res, next) => {
-      if(bareServer.shouldRoute(req)) bareServer.routeRequest(req, res); else next();
+      if (bareServer.shouldRoute(req)) bareServer.routeRequest(req, res);
+      else next();
     });
 
-    server.middlewares.use("/URIconfig", (req, res) => {
-      res.end(JSON.stringify({
-        DC: process.env['INVITE_URL'] || "example.com",
-        CH: process.env['CHATBOX_URL'] || "example.com"
-      }));
+    server.middlewares.use('/URIconfig', (req, res) => {
+      res.end(
+        JSON.stringify({
+          DC: process.env['INVITE_URL'] || 'example.com',
+          CH: process.env['CHATBOX_URL'] || 'example.com',
+        })
+      );
     });
 
-    server.middlewares.use(express.static("./static"));
+    server.middlewares.use(express.static('./static'));
 
     // Custom resolver to use DNS filtering service
-    const originalLookup = dns.lookup;
+    const originalCreateConnection = net.createConnection;
     const dnsServer = process.env.DNS || '1.1.1.1';
-    dns.lookup = function(hostname, options, callback) {
-      const callbackWrapper = (err, address, family) => {
-        if (err) {
-          console.error(`DNS lookup failed for ${hostname}`);
-          res.status(403).sendFile('./blocked.html', { root: './static' });
-        } else {
-          callback(err, address, family);
-        }
+
+    net.createConnection = function (options, callback) {
+      const originalOnLookup = options.lookup;
+      options.lookup = function (hostname, options, callback) {
+        const callbackWrapper = (err, address, family) => {
+          if (err) {
+            console.error(`DNS lookup failed for ${hostname}`);
+            res.status(403).sendFile('./blocked.html', { root: './static' });
+          } else {
+            callback(err, address, family);
+          }
+        };
+        originalCreateConnection.call(net, { ...options, host: dnsServer }, () => {
+          originalOnLookup.call(null, hostname, options, callbackWrapper);
+        });
       };
-      originalLookup.call(dns, hostname, { ...options, server: [dnsServer] }, callbackWrapper);
+      return originalCreateConnection.call(net, options, callback);
     };
-  }
+  },
 };
 
 // https://vitejs.dev/config/
@@ -54,8 +63,8 @@ export default defineConfig({
         },
         titleProp: true,
         ref: true,
-      }
+      },
     }),
-    setupProxy
-  ]
+    setupProxy,
+  ],
 });

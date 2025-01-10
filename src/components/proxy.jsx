ab109f468c522076c242a117a5b5512067c07aff
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2025-01-10 14:25:17 -0600

    Organization.

--- src/components/proxy.jsx
+++ src/components/proxy.jsx
@@ -36,6 +36,66 @@ async function workingIcon(bare, list) {
 }
 
 /**
+ * Functions needed for Adult Filtering
+ */
+function xorDecode(str, key) {
+  const keyBytes = key.match(/.{2}/g).map(byte => parseInt(byte, 16));
+
+  let decoded = '';
+  for (let i = 0; i < str.length; i++) {
+      decoded += String.fromCharCode(str.charCodeAt(i) ^ keyBytes[i % keyBytes.length]);
+  }
+  return decoded;
+}
+
+function urlDecodeAndXor(url2) {
+    let decodedUrl = decodeURIComponent(url2);
+    
+    let xorDecodedUrl = xorDecode(decodedUrl, atob(atob(atob('VFVSQmQwMXFRWGROUkVrOQ'))));
+    
+    return xorDecodedUrl;
+}
+
+async function isFiltered(hostname) {
+  try {
+    const options = {
+      method: 'GET',
+      headers: { accept: 'application/dns-json' }
+    };
+
+    var response = await fetch(`https://family.cloudflare-dns.com/dns-query?name=${hostname}`, options);
+    var data = await response.json();
+
+    if (data && Array.isArray(data.Comment) && data.Comment.some(comment => comment.includes("Filtered"))) {
+      return true; // Site is filtered (NSFW)
+    } else {
+      return false; // Site is not filtered (SFW)
+    }
+  } catch (error) {
+    console.error('Error occurred while checking hostname:', error);
+    return false; // Default to false if an error occurs
+  }
+}
+const BlockedHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Not Permitted Website</title>
+    <style>
+        body { font-family: Arial, sans-serif; text-align: center; padding: 20px; }
+        h1 { color: red; }
+    </style>
+</head>
+<body>
+    <h1>The site you were trying to access is not permitted by the authors of this website.</h1>
+</body>
+</html>
+`;
+
+
+/**
  * Loads an image. If the image src is a blob, the blob will be revoked upon dismount.
  */
 function BareIcon({ src, ...attributes }) {
@@ -132,91 +192,38 @@ var Proxy = React.forwardRef(({ overrideWindow }, ref) => {
           updatedConfig.title =
             web.current.contentWindow.document.title ||
             getWindowLocation(web.current);
+
+          /**
+          *  Adult Filtering Logic
+          */
           console.log("[135] Navigating to Link: " + getWindowLocation(web.current));
-          function xorDecode(str, key) {
-              const keyBytes = key.match(/.{2}/g).map(byte => parseInt(byte, 16));
           
-              let decoded = '';
-              for (let i = 0; i < str.length; i++) {
-                  decoded += String.fromCharCode(str.charCodeAt(i) ^ keyBytes[i % keyBytes.length]);
-              }
-              return decoded;
-            }
-            
-            function urlDecodeAndXor(url2) {
-                let decodedUrl = decodeURIComponent(url2);
-                
-                let xorDecodedUrl = xorDecode(decodedUrl, atob(atob(atob('VFVSQmQwMXFRWGROUkVrOQ'))));
-                
-                return xorDecodedUrl;
-            }
-            
-            async function isFiltered(hostname) {
-              try {
-                const options = {
-                  method: 'GET',
-                  headers: { accept: 'application/dns-json' }
-                };
-            
-                var response = await fetch(`https://family.cloudflare-dns.com/dns-query?name=${hostname}`, options);
-                var data = await response.json();
-            
-                if (data && Array.isArray(data.Comment) && data.Comment.some(comment => comment.includes("Filtered"))) {
-                  return true; // Site is filtered (NSFW)
-                } else {
-                  return false; // Site is not filtered (SFW)
-                }
-              } catch (error) {
-                console.error('Error occurred while checking hostname:', error);
-                return false; // Default to false if an error occurs
-              }
-            }
-            
-            var url = getWindowLocation(web.current)
+          var url = getWindowLocation(web.current)
+        
+          var result = "";
           
-            var result = "";
-            
-            if (url.includes("/data-load-uv.html#")) {
-                result = atob(url.replace(window.location.hostname + "/data-load-uv.html#", "").replace("https://","").replace("http://",""));
-            } else if (url.includes(window.location.hostname)) {
-                result = urlDecodeAndXor(url.replace(/.*\/(rho\/|co\/|sw-src\/)/, ""));
-            } else {
-                result = url;
+          if (url.includes("/data-load-uv.html#")) {
+              result = atob(url.replace(window.location.hostname + "/data-load-uv.html#", "").replace("https://","").replace("http://",""));
+          } else if (url.includes(window.location.hostname)) {
+              result = urlDecodeAndXor(url.replace(/.*\/(rho\/|co\/|sw-src\/)/, ""));
+          } else {
+              result = url;
+          }
+          
+          var host = new URL(result).hostname;
+          
+          console.log(host);
+          
+          isFiltered(host).then((result) => {
+            if (result) {
+              console.log(`${host} is filtered (NSFW).`);
+              document.open();
+              document.write(BlockedHTML);
+              document.close();
             }
-            
-            var host = new URL(result).hostname;
-            
-            console.log(host);
-            
-            const BlockedHTML = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Not Permitted Website</title>
-                <style>
-                    body { font-family: Arial, sans-serif; text-align: center; padding: 20px; }
-                    h1 { color: red; }
-                </style>
-            </head>
-            <body>
-                <h1>The site you were trying to access is not permitted by the authors of this website.</h1>
-            </body>
-            </html>
-            `;
-            
-            
-            isFiltered(host).then((result) => {
-              if (result) {
-                console.log(`${host} is filtered (NSFW).`);
-                document.open();
-                document.write(BlockedHTML);
-                document.close();
-              }
-            }).catch(error => {
-              console.error('Error occurred while checking filter status of hostname:', error);
-            });
+          }).catch(error => {
+            console.error('Error occurred while checking filter status of hostname:', error);
+          });
           
           var icon = web.current.contentWindow.document.querySelector(
             "link[rel*='icon'],link[rel='shortcut icon']"
